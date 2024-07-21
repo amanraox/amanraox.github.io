52bbf1608b186767a9be9e34c061df4e44666444
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-07-21 07:57:51 +0530

    Add files via upload

---
+++ js/cmds/cp.js
@@ -0,0 +1,4 @@
+export default function(output) {
+  output.innerHTML += '<br><p align="left">elsleldwl;xh5          <a href="https://leetcode.com/u/raoxaman/" target="_blank" rel="noopener noreferrer"><img height="315em" src="https://leetcard.jacoblin.cool/raoxaman?border=1.5&font=Ubuntu&radius=13&ext=contest&sheets=https://gist.githubusercontent.com/amanraox/cf6cdd10f40353086bcb2de6694a4ce2/raw/786c10c0174e05e10c866f42ba0d47acf391b42a/leetcode_stats_card.css"/></a><a href="https://codeforces.com/profile/amanloves69" target="_blank" rel="noopener noreferrer"><img height="317em" src="https://codeforces-readme-stats.vercel.app/api/card?username=amanloves69&title_color=00ff99&text_color=fff&border_color=00ff99&bg_color=121217&icon_color=00ff99" /></a></p><br>';
+}
+
