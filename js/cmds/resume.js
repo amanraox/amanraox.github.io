6bd1d83bb5e2e5d1169e60e9809d7c7e8944c402
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-08-14 03:02:29 +0530

    Update resume.js

--- js/cmds/resume.js
+++ js/cmds/resume.js
@@ -1,5 +1,13 @@
 window.open('https://amanraox.vercel.app/amanCV.pdf', '_blank');
 export default function(output) {
     output.innerHTML += 'Check the tab on the right containg <span data-color="cyan">Resume</span>';
-    window.open('https://amanraox.vercel.app/amanCV.pdf');
+    // window.open('https://amanraox.vercel.app/amanCV.pdf');
+    const link = document.createElement('a');
+    link.href = 'https://amanraox.vercel.app/amanCV.pdf';
+    link.download = 'amanCV.pdf'; // Suggest a filename for the download
+
+    // Trigger a click on the anchor element
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 }
